7f9141bce43b37451283d9ae5726bbf7bab471eb
Leonardo Alves de Melo <leonardo.alves.melo.1995@gmail.com>
2018-06-24 20:10:36 -0300

    Primeiro commit com a rede treinando corretamente. Falta melhorar parâmetros para deixar mais rápido.

--- player_2048.py
+++ player_2048.py
@@ -1,26 +1,52 @@
 from game_2048 import *
 import math
 
-game = Game()
+
 
 directions = ['u', 'd', 'l', 'r']
 
 def play_with_AI(nn):
 
+	game = Game()
+
 	playing = True
+
+	play = 1
+
+	#print '\n\n------------------\n--------------------\n'
 
 	while playing:
 
-		entrance = game.board.reshape(-1)
+		entrance = np.copy(game.board)
+		entrance = entrance.reshape(-1)
+		#print game.board
+
+		#print 'play ' + str(play)
+		play += 1
 
 		for index in range(len(entrance)):
 			if entrance[index] != 0:
 				entrance[index] = math.log(entrance[index], 2)
 
-		movement = directions[nn.predict(entrance)]
+		maior = 0
+		saida = nn.sactivate(entrance)
+		for i in range(len(saida)):
+			if saida[i] > saida[maior]:
+				maior = i
+
+		movement = directions[maior]
+
+		#print movement
+
+		#print 'O meu movimento foi: ' + movement
+
+
 
 		if not game.apply_command(movement):
-			return game.score
+			#print '++++++++++++++++++\n+++++++++++++++++\n\n\n'
+			return float(game.score)
+
+		game.new_round()
 
 
 def play_with_user():
--- trainer_2048.py
+++ trainer_2048.py
@@ -1,4 +1,68 @@
 from player_2048 import *
+from neat import config, population, chromosome, genome, visualize
+from neat.nn import nn_pure as nn
+
+config.load('2048_config')
+
+max_generations = 1000
+
+# set node gene type
+chromosome.node_gene_type = genome.NodeGene
+
+# XOR-2
+#INPUTS = [[0, 0], [0, 1], [1, 0], [1, 1]]
+#OUTPUTS = [0, 1, 1, 0]
+
+def eval_fitness(population):
+
+    #global melhor_score
+
+    for chromo in population:
+        net = nn.create_ffphenotype(chromo)
+
+        #error = 0.0
+        #error_stanley = 0.0
+        #for i, inputs in enumerate(INPUTS):
+        #net.flush() # not strictly necessary in feedforward nets
+
+        score = play_with_AI(net)
+
+        #if score > melhor_score:
+        #    melhor_score = score
+
+        #print 'Melhor: ------------- ' + str(int(melhor_score))
+
+        #print 'olha esse score:'
+        #print score
 
 
-play_with_user()
+        #output = net.sactivate(inputs) # serial activation
+        #error += (output[0] - OUTPUTS[i])**2
+
+        #error_stanley += math.fabs(output[0] - OUTPUTS[i])
+
+        #chromo.fitness = (4.0 - error_stanley)**2 # (Stanley p. 43)
+        #chromo.fitness = 1 - math.sqrt(error/len(OUTPUTS))
+        chromo.fitness = score
+
+population.Population.evaluate = eval_fitness
+
+pop = population.Population()
+pop.epoch(max_generations, report=True, save_best=False)
+
+winner = pop.stats[0][-1]
+print('Number of evaluations: %d' %winner.id)
+
+# Visualize the winner network (requires PyDot)
+visualize.draw_net(winner) # best chromosome
+
+# Plots the evolution of the best/average fitness (requires Biggles)
+visualize.plot_stats(pop.stats)
+# Visualizes speciation
+visualize.plot_species(pop.species_log)
+
+# Let's check if it's really solved the problem
+print('\nBest network output:')
+brain = nn.create_ffphenotype(winner)
+score = play_with_AI(brain)
+print 'score do melhor: ' + str(int(score))
